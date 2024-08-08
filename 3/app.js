bc3d6f5ef01016dd84990dd935ab320d7ffc96ed
L0R3NZ0-L30Z <139158059+L0R3NZ0-L30Z@users.noreply.github.com>
2024-08-08 20:41:06 -0300

    day home

--- 3/app.js
+++ 3/app.js
@@ -1,18 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
+
       enteredGoalValue: '',
-      goals: []
+      goals: [],
+      goalsTime: []
     };
   },
   methods: {
     addGoal() {
-      return this.goals.push(this.enteredGoalValue);
-    }
-  },
+      if(this.enteredGoalValue.length > 0 & this.enteredGoalValue.length < 20){
+        const varc = this.goals.push(this.enteredGoalValue);
+        this.enteredGoalValue = '';
+        return enteredGoalValue;}
+    },
   removeG(indx) {
     this.goals.splice(indx, 1);
-  }
+  },
+  localTime(indx) {
+    return this.goalsTime[indx].toLocaleTimeString();
+  },
+  
+  },
 });
 
 app.mount('#user-goals');
